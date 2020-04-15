ddc924e1e5ef44323bea389ea8848b4f05fa7511
Paolo <paololabsplus@gmail.com>
2020-04-14 22:52:12 -0400

    Adding methos push and navitate

--- navigation/App.js
+++ navigation/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from  'react-navigation-stack';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title='Ir a detalle'
+        onPress={() => navigation.navigate('Detalle', { userId: 2})}
+      />
     </View>
   )
 }
 
-const DetalleScreen = () => {
+const DetalleScreen = ({ navigation }) => {
+
+  const lala = navigation.getParam('lala', 'valor por defecto');
+
   return (
     <View style={styles.container}>
-      <Text>Soy la pantalla de detalle</Text>
+      <Text>Soy la pantalla de detalle {lala}</Text>
+      <Button
+        title='volver'
+        onPress={() => navigation.goBack()}
+      />
     </View>
   )
 };
@@ -26,7 +37,7 @@ const AppNavigator = createStackNavigator({
   Detalle: {
     screen:  DetalleScreen
   }
-}, { initialRouteName: 'Detalle' } );
+}, { initialRouteName: 'Home' } );
 
 export default createAppContainer(AppNavigator);
 
