791dc0dea43668bebd3c30e656552ee48c4256e0
Paolo <paololabsplus@gmail.com>
2020-04-16 12:06:22 -0400

    Adding header Style

--- navigation/App.js
+++ navigation/App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from  'react-navigation-stack';
 
+const Logo = () => <Text>Lalala</Text>
+
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
@@ -15,20 +17,42 @@ const HomeScreen = ({ navigation }) => {
   )
 }
 
+HomeScreen.navigationOptions = {
+  // title: 'Home',
+  headerTitle: () => <Logo />,
+  headerStyle: { 
+    backgroundColor: '#222'
+   },
+  //  headerTintColor: '#fff', //cambia color de texto
+  //  headerTitleStyle: {
+  //    fontWeight: '500'
+  //  }
+}
+
 const DetalleScreen = ({ navigation }) => {
 
-  const lala = navigation.getParam('lala', 'valor por defecto');
+  const lala = navigation.getParam('lala', 'valor por defecto'); // cao de que no exista
 
   return (
     <View style={styles.container}>
       <Text>Soy la pantalla de detalle {lala}</Text>
       <Button
         title='volver'
-        onPress={() => navigation.goBack()}
+        onPress={() => navigation.setParams({ title: 'Usuario 1' })}
       />
     </View>
   )
 };
+
+DetalleScreen.navigationOptions = ({ navigation, navigationOptions }) => {
+  return {
+    title: navigation.getParam('title', 'Cargando....'),
+    headerStyle: {
+      backgroundColor: navigationOptions.headerStyle.backgroundColor
+    }
+    // headerTintColor: '#5e5', //cambia color de texto
+  }
+}
 
 const AppNavigator = createStackNavigator({
   Home: {
@@ -37,7 +61,20 @@ const AppNavigator = createStackNavigator({
   Detalle: {
     screen:  DetalleScreen
   }
-}, { initialRouteName: 'Home' } );
+}, 
+{ 
+  initialRouteName: 'Home',
+  defaultNavigationOptions: { 
+    // Todos los parametros
+    headerStyle: {
+      backgroundColor: '#fec',
+    },
+    headerTintColor: '#555',
+    headerTitleStyle: {
+      fontWeight: '900',
+    }
+  }
+});
 
 export default createAppContainer(AppNavigator);
 
